c7feef792e815d903755da06cd7411bc59e7361d
MehdiUI <mehdi.lemaire.etu@gmail.com>
2025-10-08 10:39:58 +0200

    try avec le lien github

--- vite.config.ts
+++ vite.config.ts
@@ -9,20 +9,5 @@ export default defineConfig({
       '@': path.resolve(__dirname, './src'),
     },
   },
-  
-  
-  base: './',
-  
-  build: {
-
-    outDir: 'dist',
-    
-
-    assetsDir: 'assets',
-    
-
-    emptyOutDir: true,
-  },
-  
- 
+  base: 'ClimaSphere', 
 })
