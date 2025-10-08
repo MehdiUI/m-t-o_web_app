bce804f15701e5392afe96810f286329812a8208
MehdiUI <mehdi.lemaire.etu@gmail.com>
2025-10-08 11:21:40 +0200

    layout à l'accueil /webapp

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -12,7 +12,7 @@ const Header = () => {
   return (
     <header className="sticky top-0 z-50 w-full border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60 py-2">
       <div className="container mx-auto flex h-16 items-center justify-between px-4">
-       <Link to={"/"}>
+       <Link to={"./"}>
           <p className={`text-2xl font-bold ${
             isDark 
               ? 'text-white' 
